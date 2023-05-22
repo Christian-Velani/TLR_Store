b3b37023897299ef8de3873048bf24b6e4f0198a
Christian Velani <chris13082000@gmail.com>
2023-05-22 06:50:28 -0300

    Criando a interface para o gênero

---
+++ Repositories/Interface/IGeneroRepository.cs
@@ -0,0 +1,8 @@
+public interface IGeneroRepository
+{
+    List<Genero> BuscarListaPorJogo(int idJogo);
+    List<Genero> BuscarListaCompleta();
+    Genero Buscar(int idGenero);
+    void Cadastrar(Genero genero);
+    void Atualizar(int idGenero, Genero genero);
+}
