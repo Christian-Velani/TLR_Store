48fa6827abdd4e171f52cbc5671ab230466e377e
Christian Velani <chris13082000@gmail.com>
2023-05-22 07:12:49 -0300

    Criei o GeneroRepository

---
+++ Repositories/Repository/GeneroRepository.cs
@@ -0,0 +1,103 @@
+using System.Data.SqlClient;
+
+public class GeneroRespository : Database, IGeneroRepository
+{
+    public void Atualizar(int idGenero, Genero genero)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        cmd.CommandText = @"UPDATE GENERO
+                            SET nome = @nome
+                            WHERE idGenero = @id";
+
+        cmd.Parameters.AddWithValue("@nome", genero.NomeGenero);
+        cmd.Parameters.AddWithValue("@id", idGenero);
+
+        cmd.ExecuteNonQuery();
+    }
+
+    public Genero Buscar(int idGenero)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        cmd.CommandText = @"SELECT idGenero, nome
+                            FROM GENEROS
+                            WHERE idGenero = @id";
+        
+        cmd.Parameters.AddWithValue("@id", idGenero);
+
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        if(reader.Read())
+        {
+            Genero genero = new Genero();
+            genero.IdGenero = Convert.ToInt32(reader["idGenero"]);
+            genero.NomeGenero = reader["nome"].ToString();
+
+            return genero;
+        }
+
+        return null;
+    }
+
+    public List<Genero> BuscarListaCompleta()
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        cmd.CommandText = @"SELECT * FROM GENEREOS";
+
+        List<Genero> generos = new List<Genero>();
+
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        while(reader.Read())
+        {
+            Genero genero = new Genero();
+            genero.IdGenero = Convert.ToInt32(reader["idGenero"]);
+            genero.NomeGenero = reader["nome"].ToString();
+
+            generos.Add(genero);
+        }
+
+        return generos;
+    }
+
+    public List<Genero> BuscarListaPorJogo(int idJogo)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        cmd.CommandText = @"SELECT G.idGenero, G.nome
+                            FROM GENEROS G
+                            INNER JOIN JOGOS_GENEROS JG
+                            ON G.idGenero = JG.generoId
+                            WHERE JG.jogoId = @id";
+
+        cmd.Parameters.AddWithValue("@id", idJogo);
+
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        List<Genero> generos = new List<Genero>();
+
+        while(reader.Read())
+        {
+            Genero genero = new Genero();
+            genero.IdGenero = Convert.ToInt32(reader["idGenero"]);
+            genero.NomeGenero = reader["nome"].ToString();
+
+            generos.Add(genero);
+        }
+
+        return generos;
+    }
+
+    public void Cadastrar(Genero genero)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        cmd.CommandText = @"INSERT INTO GENEROS(nome) VALUES(@nome)";
+
+        cmd.Parameters.AddWithValue("@nome", genero.NomeGenero);
+
+        cmd.ExecuteNonQuery();
+    }
+}
