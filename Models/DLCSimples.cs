6d9ea0a80dc4743d10adac516da8e787b9d302eb
Christian Velani <chris13082000@gmail.com>
2023-06-03 15:28:21 -0300

    Criei algumas models mais simples

---
+++ Models/DLCSimples.cs
@@ -0,0 +1,6 @@
+public class DLCSimples
+{
+    public int IdComplemento { get; set; }
+    public byte[] Imagem { get; set; }
+    public string NomeComplemento { get; set; }
+}
---
+++ Models/JogoSimples.cs
@@ -0,0 +1,6 @@
+public class JogoSimples
+{
+    public int JogoId { get; set; }
+    public byte[] Imagem { get; set; }
+    public string Nome { get; set; }
+}
